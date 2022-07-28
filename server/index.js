b75e31acabaf0e4bc3655c930b70e441c45cdda6
koomekevo <koome.kevo@gurucrafts.net>
2022-07-28 11:07:45 +0300

    Add graphqlHTTP args

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 5000;
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-
+    schema, 
+    graphiql: process.env.NODE_ENV === 'development'
 }))
 
 app.listen(port, console.log(`Server running on port ${port}`));
