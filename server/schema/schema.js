1fea33e5478e9dafaa0e68ded65aab20a2078190
koomekevo <koome.kevo@gurucrafts.net>
2022-07-29 10:02:09 +0300

    Add projects and project to RootQuery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -27,20 +27,33 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            },
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find(project => project.id === args.id);
+            },
+        },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
                 return clients;
-            }
+            },
         },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return clients.find(client => client.id === args.id);
-            }
-        }
-    }
+            },
+        },
+    },
 })
 
 module.exports = new GraphQLSchema({
