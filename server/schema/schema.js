3c5de843e9a7a44afb3e7ebb60128950aa54baa9
koomekevo <koome.kevo@gurucrafts.net>
2022-07-29 08:56:15 +0300

    Add ClientType GraphQLObjectType

---
+++ server/schema/schema.js
@@ -0,0 +1,14 @@
+const { projects, clients } = require('../sampleData');
+
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString }
+    })
+});
