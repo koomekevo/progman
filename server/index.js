91a4b8d3fc74158446e550b5122e74fea04b8b28
koomekevo <koome.kevo@gurucrafts.net>
2022-07-30 10:59:46 +0300

    Add connectDB() to index.js

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const colors = require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+
+// Connect to database
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema, 
