998cd7d5dcb33149a1a57661cef7504fd0d6c1a2
koomekevo <koome.kevo@gurucrafts.net>
2022-07-30 12:14:58 +0300

    Add resolve in mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -81,6 +81,18 @@ const mutation = new GraphQLObjectType({
             type: ClientType,
             args: {
                 name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                const client = new Client ({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone,
+                });
+                return client.save();
+
+                Client.create
             }
         }
     }
