1973de889abc6b7280dc3ff4ca68ecad74006383
koomekevo <koome.kevo@gurucrafts.net>
2022-07-30 11:38:09 +0300

    Modify return statements in RootQuery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -40,27 +40,27 @@ const RootQuery = new GraphQLObjectType({
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return projects;
+               return Project.find();
             },
         },
         project: {
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return projects.find(project => project.id === args.id);
+                return Project.findById(args.id);
             },
         },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
-                return clients;
+                return Client.find();
             },
         },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
+                return Client.findById(args.id);
             },
         },
     },
