dd580d2f450e4b8e4d6ae1e25add10193261803d
koomekevo <koome.kevo@gurucrafts.net>
2022-07-31 15:36:25 +0300

    Change from exampleModal to addClientModal

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -4,6 +4,51 @@ import { useMutation } from "@apollo/client";
 
 export default function AddClientModal() {
   return (
-    <div>AddClientModal</div>
-  )
+    <>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        data-bs-toggle="modal"
+        data-bs-target="#addClientModal"
+      >
+        Add Client
+      </button>
+
+      <div
+        className="modal fade"
+        id="addClientModal"
+        aria-labelledby="addClientModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="addClientModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
