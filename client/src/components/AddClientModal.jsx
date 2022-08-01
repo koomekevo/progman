77a894e9b69a290f02d7da1cf0541e7eb730f758
koomekevo <koome.kevo@gurucrafts.net>
2022-08-01 05:45:47 +0300

    use addClient mutation in AddClientModal.jsx

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
+import { ADD_CLIENT } from "./mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 export default function AddClientModal() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.concat([addClient]) },
+      });
+    },
+  });
+
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(name, email, phone);
-  }
-  
+  };
+
   return (
     <>
       <button
@@ -36,7 +50,7 @@ export default function AddClientModal() {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="addClientModalLabel">
-                Modal title
+                Add Client
               </h5>
               <button
                 type="button"
@@ -77,7 +91,13 @@ export default function AddClientModal() {
                     onChange={(e) => setPhone(e.target.value)}
                   />
                 </div>
-                <button type="submit" data-bs-dismiss="modal" className="btn btn-secondary">Submit</button>
+                <button
+                  type="submit"
+                  data-bs-dismiss="modal"
+                  className="btn btn-secondary"
+                >
+                  Submit
+                </button>
               </form>
             </div>
           </div>
