9fbb5af77ca4977b60b584cbea54235ab890882a
koomekevo <koome.kevo@gurucrafts.net>
2022-08-01 10:05:04 +0300

    Add Projects.jsx

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,18 @@
+import Spinner from "./Spinner";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECTS } from '../queries/projectQueries';
+
+export default function Projects() {
+    const { loading, error, data } = useQuery(GET_PROJECTS);
+
+    if (loading) return <Spinner />;
+    if (error) return <p>Something went wrong</p>;
+
+    return <>
+        { data.projects.length > 0 ? (
+            <div className="row">
+                { data.projects.map() }
+            </div>
+        ) : (<p>No Projects</p>) }
+        </>; 
+}
